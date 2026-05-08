fb4ecd33f70e58df32756d9a0c69fdf391838afa
Kashish <kash1ish2114@gmail.com>
2026-05-08 21:29:07 +0530

    add jwt protected routes middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.get("/profile", protect, (req, res) => {
+  res.json(req.user)
+})
 router.post("/login", loginUser);
 
 export default router;

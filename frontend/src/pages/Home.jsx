ace80eefbd5e95d7fb3173c22576209594d54924
Kashish <kash1ish2114@gmail.com>
2026-05-12 14:26:10 +0530

    setup react frontend with routing and tailwind

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+
+  return (
+
+    <div className="p-10">
+
+      <h1 className="text-5xl font-bold">
+        Welcome to LMS
+      </h1>
+
+    </div>
+
+  )
+
+}
+
+export default Home
